bd0dbc2db609929e8a4c346b97eb5c3f064f3dc3
younes BENNACEUR <younesbennaceur06@gmail.com>
2025-06-14 00:08:20 +0100

    fix deployment error

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Hero from '../assets/hero.png';
-import BackgroundImage from '../assets/backgroundimage.png'; // Ton image de fond
+import BackgroundImage from '../assets/backgroundImage.png'; // Ton image de fond
 import note from '../assets/note.png';
 import { NavLink } from 'react-router-dom';
 
